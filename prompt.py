08e578259b8d16a57960a485a5a232ed5d209722
minh21b <minh21b@gmail.com>
2024-03-06 16:28:08 -0500

    aaa

--- prompt.py
+++ prompt.py
@@ -1,10 +1,5 @@
-def report_count(data):
-    word_count = {}
-    data = data.split()
-    for word in data:
-        word_count[word] = word_count.get(word, 0) + 1
-    #token = token
-    return word_count
+def report_count(token):
+    return word_count.get(token)
 
 def word_count(data):
     word_count = {}
