e5edc85f65025956e8ad03ef5e9723c94987c1da
minh21b <minh21b@gmail.com>
2024-03-06 15:45:01 -0500

    dummy

--- prompt.py
+++ prompt.py
@@ -3,4 +3,4 @@ def report_count(corpus, token):
     for word in corpus:
         word_count[word] = word_count.get(word, 0) + 1
     count = word_count.get(token)
-    return count
+    return word_count
